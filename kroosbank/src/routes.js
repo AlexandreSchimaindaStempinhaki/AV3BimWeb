b25369c9d41fc38247d3bd18454c3f27567431c6
julia <cledisdasteclas@gmail.com>
2025-09-30 15:06:14 -0300

    Front End Criado

---
+++ kroosbank/src/routes.js
@@ -0,0 +1,12 @@
+// React Router
+import { createBrowserRouter, } from "react-router-dom";
+import Login from './pages/Login'
+
+const router = createBrowserRouter([
+    { 
+        path: "/", 
+        element: <Login />
+    }
+])
+
+export default router
